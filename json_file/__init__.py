fc59a53df62e66daaf2f0e354bc445145567ee96
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-24 23:49:52 -0800

    Updated docstrings

--- json_file/__init__.py
+++ json_file/__init__.py
@@ -16,20 +16,39 @@ class JSONFile(PavedPath):
     """Library for working with JSON files."""
 
     def __init__(self, *_args: PathableType) -> None:
-        """Initialize the JSONFile object."""
+        """Initialize the JSONFile class.
+
+        Args:
+        ----
+            _args: The path fragments to join together.
+        """
         self.parsed_cached_value = None
         super().__init__()
 
     def parsed(self) -> Any:  # type: ignore # noqa: PGH003, ANN401 - Impossible to know thereturn type of a loaded json
         # file so in this situation returning Any is the only logical choice
-        """Parse the file bytes using json."""
+        """Parse the file bytes using json.
+
+        Returns
+        -------
+            An uncached parsed JSON object.
+        """
         # I don't know of any reason why you would want to use read_text instead of read_bytes for JSON. Unless a
         # specific need arises this function will always just use read_bytes
         return json.loads(self.read_bytes())
 
     def parsed_cached(self, *, reload: bool = False) -> Any:  # type: ignore # noqa: PGH003, ANN401 - Impossible to know
         # the return type of a loaded json file so in this situation returning Any is the only logical choice
-        """Parse the file bytes using json and cache the result."""
+        """Parse the file bytes using json and cache the result.
+
+        Args:
+        ----
+            reload: Whether to reload the file bytes and reparse the file.
+
+        Returns:
+        -------
+            A cached parsed JSON object.
+        """
         # I don't know of any reason why you would want to use read_text instead of read_bytes for JSON. Unless a
         # specific need arises this function will always just use read_bytes
         if not self.parsed_cached_value or reload:
